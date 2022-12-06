a0f3d7ae14243392ba4405db368c48c08d19b397
jtr354 <jtr354@foxmail.com>
2022-12-06 15:56:01 +0800

    fix: symbol.for

--- packages/shared/ReactSymbols.ts
+++ packages/shared/ReactSymbols.ts
@@ -1,7 +1,5 @@
-// const supportSymbol = typeof Symbol === 'function' && Symbol.for;
+const supportSymbol = typeof Symbol === 'function' && Symbol.for;
 
-// export const REACT_ELEMENT_TYPE = supportSymbol
-// 	? Symbol('react.element')
-// 	: 0xce7;
-
-export const REACT_ELEMENT_TYPE = 0xce7;
+export const REACT_ELEMENT_TYPE = supportSymbol
+	? Symbol.for('react.element')
+	: 0xce7;
